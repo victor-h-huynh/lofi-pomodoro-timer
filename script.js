39ddc217e4035b0860b149e25237376a7442f4aa
victor-h-huynh <victor.h.huynh@gmail.com>
2022-02-02 12:05:50 -0500

    start-functionality

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 "use strict";
+/////////////////////////////////////////////////////////////////////
 // Elements
+const startBtn = document.querySelector(".start__btn");
+const stopBtn = document.querySelector(".stop__btn");
+const resetBtn = document.querySelector(".reset__btn");
+const timer = document.querySelector(".timer");
 
+let timerId;
+
+/////////////////////////////////////////////////////////////////////
 // Functions
+const startTimer = function () {
+  let time = 3;
+
+  const timerId = setInterval(() => {
+    let mins = String(Math.trunc(time / 60)).padStart(2, 0);
+    let secs = String(time % 60).padStart(2, 0);
+
+    if (secs === 0 && mins > 0) {
+      mins--;
+      secs--;
+    } else {
+      time--;
+    }
+    timer.innerHTML = `${mins}:${secs}`;
+    console.log(mins, secs, time);
+    if (time === -1) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+
+  return timerId;
+};
+
+/////////////////////////////////////////////////////////////////////
+// Event Handlers
+
+// Start Button
+startBtn.addEventListener("click", function () {
+  timerId = startTimer();
+});
+
+stopBtn.addEventListener("click", function () {
+  clearInterval(timerId);
+});
